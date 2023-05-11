a53c2ee882665fe3311e4858a77253c86e9e7991
genki-ryukyu <e215732@ie.u-ryukyu.ac.jp>
2023-05-11 14:03:04 +0900

    5th commit

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-import dataset 
+import dataset
+import regression
+ 
 X, Y = dataset.load_linear_example()
 
 print(f'{X=}')
 print(f'{Y=}')
+
+model = regression.LinearRegression()
+print(model.x)
+
 
