ef3e2df5db3f6761416678457426e3112fe789d8
genki-ryukyu <e215732@ie.u-ryukyu.ac.jp>
2023-05-11 13:57:00 +0900

    4th commit

---
+++ regression.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class LinearRegression:
+    x = 1
+    theta = None
+    y = None
+    
+    def fit(self,x,y):
+        pass
+    
+    def predict(self,x):
+        pass
+    def score(self,x,y):
+        pass
--- test.py
+++ test.py
@@ -3,3 +3,4 @@ X, Y = dataset.load_linear_example()
 
 print(f'{X=}')
 print(f'{Y=}')
+
