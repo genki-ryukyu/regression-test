b0dc17b947c32548c6d1f56e29339f829c9f000e
genki-ryukyu <e215732@ie.u-ryukyu.ac.jp>
2023-05-11 14:07:40 +0900

    6th commit

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 import dataset
 import regression
+import importlib
+
  
 X, Y = dataset.load_linear_example()
-
+#datasetのテスト
 print(f'{X=}')
 print(f'{Y=}')
-
+#regressionのver.1テスト
 model = regression.LinearRegression()
 print(model.x)
+
+#regressionのver.2テスト
+model.fit(X,Y)
+print(model.theta)
 
 
